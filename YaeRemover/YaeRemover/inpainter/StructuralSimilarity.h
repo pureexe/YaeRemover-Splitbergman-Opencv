eb15a661e8de504631fcb3e05a2e56a7cdc200fa
Pakkapon Phongthawee <me@pureapp.in.th>
2018-10-12 11:29:57 +0700

    Save borrowframe technique

---
+++ YaeRemover/YaeRemover/inpainter/StructuralSimilarity.h
@@ -0,0 +1,55 @@
+#pragma once
+#include "../stdafx.h"
+
+Scalar StructuralSimilarity(const Mat& i1, const Mat& i2)
+{
+	const double C1 = 6.5025, C2 = 58.5225;
+	/***************************** INITS **********************************/
+	int d = CV_32F;
+
+	Mat I1, I2;
+	i1.convertTo(I1, d);           // cannot calculate on one byte large values
+	i2.convertTo(I2, d);
+
+	Mat I2_2 = I2.mul(I2);        // I2^2
+	Mat I1_2 = I1.mul(I1);        // I1^2
+	Mat I1_I2 = I1.mul(I2);        // I1 * I2
+
+	/*************************** END INITS **********************************/
+
+	Mat mu1, mu2;   // PRELIMINARY COMPUTING
+	GaussianBlur(I1, mu1, Size(11, 11), 1.5);
+	GaussianBlur(I2, mu2, Size(11, 11), 1.5);
+
+	Mat mu1_2 = mu1.mul(mu1);
+	Mat mu2_2 = mu2.mul(mu2);
+	Mat mu1_mu2 = mu1.mul(mu2);
+
+	Mat sigma1_2, sigma2_2, sigma12;
+
+	GaussianBlur(I1_2, sigma1_2, Size(11, 11), 1.5);
+	sigma1_2 -= mu1_2;
+
+	GaussianBlur(I2_2, sigma2_2, Size(11, 11), 1.5);
+	sigma2_2 -= mu2_2;
+
+	GaussianBlur(I1_I2, sigma12, Size(11, 11), 1.5);
+	sigma12 -= mu1_mu2;
+
+	///////////////////////////////// FORMULA ////////////////////////////////
+	Mat t1, t2, t3;
+
+	t1 = 2 * mu1_mu2 + C1;
+	t2 = 2 * sigma12 + C2;
+	t3 = t1.mul(t2);              // t3 = ((2*mu1_mu2 + C1).*(2*sigma12 + C2))
+
+	t1 = mu1_2 + mu2_2 + C1;
+	t2 = sigma1_2 + sigma2_2 + C2;
+	t1 = t1.mul(t2);               // t1 =((mu1_2 + mu2_2 + C1).*(sigma1_2 + sigma2_2 + C2))
+
+	Mat ssim_map;
+	divide(t3, t1, ssim_map);      // ssim_map =  t3./t1;
+
+	Scalar mssim = mean(ssim_map); // mssim = average of ssim map
+	return mssim;
+}
--- YaeRemover/YaeRemover/inpainter/uSolver.h
+++ YaeRemover/YaeRemover/inpainter/uSolver.h
@@ -9,65 +9,68 @@ Mat uSolver(Mat u, Mat w, Mat original_image, Mat b, Mat lambda, double theta, d
 	int rowsize = width * channel;
 	int i, j, k;
 	Mat divergence = Divergence(w - b);
-	//Data conversation
-	double* _u = (double*)u.data;
-	double* _d = (double*)divergence.data;
-	double* _f = (double*)original_image.data;
-	double* _l = (double*)lambda.data;
-	double* _u_prev; //previous row
-	double* _u_next; //next row
-	_u_next = _u + rowsize;
-	//top - left corner
-	for (i = 0; i < channel; i++) {
-		_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u[i], _u[i + channel], _u[i], _u_next[i]);
-	}
-	//top-bar line
-	for (i = channel; i < rowsize - channel; i++) {
-		_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u[i - channel], _u[i + channel], _u[i], _u_next[i]);
-	}
-	//top - right corner
-	for (i = rowsize - channel; i < rowsize; i++) {
-		_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u[i - channel], _u[i], _u[i], _u_next[i]);
-	}
-	//body
-	for (k = 1; k < height - 1; k++) {
+	//for (int t = 0; t < 3; t++) {
+
+		//Data conversation
+		double* _u = (double*)u.data;
+		double* _d = (double*)divergence.data;
+		double* _f = (double*)original_image.data;
+		double* _l = (double*)lambda.data;
+		double* _u_prev; //previous row
+		double* _u_next; //next row
+		_u_next = _u + rowsize;
+		//top - left corner
+		for (i = 0; i < channel; i++) {
+			_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u[i], _u[i + channel], _u[i], _u_next[i]);
+		}
+		//top-bar line
+		for (i = channel; i < rowsize - channel; i++) {
+			_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u[i - channel], _u[i + channel], _u[i], _u_next[i]);
+		}
+		//top - right corner
+		for (i = rowsize - channel; i < rowsize; i++) {
+			_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u[i - channel], _u[i], _u[i], _u_next[i]);
+		}
+		//body
+		for (k = 1; k < height - 1; k++) {
+			// new line on memory;
+			_u_prev = _u;
+			_u += rowsize;
+			_u_next += rowsize;
+			_d += rowsize;
+			_f += rowsize;
+			_l += rowsize;
+			//body - front
+			for (i = 0; i < channel; i++) {
+				_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u_prev[i], _u[i + channel], _u[i], _u_next[i]);
+			}
+			//body - interior
+			for (i = channel; i < rowsize - channel; i++) {
+				_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u[i - channel], _u[i + channel], _u_prev[i], _u_next[i]);
+			}
+			//body - rear
+			for (i = rowsize - channel; i < rowsize; i++) {
+				_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u[i - channel], _u[i], _u[i + channel], _u_next[i]);
+			}
+		}
 		// new line on memory;
 		_u_prev = _u;
 		_u += rowsize;
-		_u_next += rowsize;
 		_d += rowsize;
 		_f += rowsize;
 		_l += rowsize;
-		//body - front
+		//bottom-left
 		for (i = 0; i < channel; i++) {
-			_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u_prev[i], _u[i + channel], _u[i], _u_next[i]);
+			_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u[i], _u[i + channel], _u_prev[i], _u[i]);
 		}
-		//body - interior
+		//bottom - bar line
 		for (i = channel; i < rowsize - channel; i++) {
-			_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u[i - channel], _u[i + channel], _u_prev[i], _u_next[i]);
+			_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u[i - channel], _u[i + channel], _u_prev[i], _u[i]);
 		}
-		//body - rear
+		//bottom-right
 		for (i = rowsize - channel; i < rowsize; i++) {
-			_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u[i - channel], _u[i], _u[i + channel], _u_next[i]);
+			_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u[i - channel], _u[i], _u_prev[i], _u[i]);
 		}
-	}
-	// new line on memory;
-	_u_prev = _u;
-	_u += rowsize;
-	_d += rowsize;
-	_f += rowsize;
-	_l += rowsize;
-	//bottom-left
-	for (i = 0; i < channel; i++) {
-		_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u[i], _u[i + channel], _u_prev[i], _u[i]);
-	}
-	//bottom - bar line
-	for (i = channel; i < rowsize - channel; i++) {
-		_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u[i - channel], _u[i + channel], _u_prev[i], _u[i]);
-	}
-	//bottom-right
-	for (i = rowsize - channel; i < rowsize; i++) {
-		_u[i] = uGaussKernel(_u[i], _f[i], _l[i], theta, omega, _d[i], _u[i - channel], _u[i], _u_prev[i], _u[i]);
-	}
+	//}
 	return u;
 }
